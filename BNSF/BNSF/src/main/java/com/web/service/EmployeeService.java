dbd386aa92ee13477380aa0c6068c955a7cccf4f
Vandy29 <vandana.a@hcl.com>
2015-11-03 15:55:04 +0530

    New Code

--- BNSF/BNSF/src/main/java/com/web/service/EmployeeService.java
+++
@@ -1,6 +0,0 @@
-package com.web.service;
-
-public interface EmployeeService {
-
-	Boolean addEmployeeToRoster(String rosterId, String employeeId, Integer sequenceNo) throws Exception;
-}
--- BNSF/BNSF/src/main/java/com/web/service/StationService.java
+++
@@ -1,8 +0,0 @@
-package com.web.service;
-
-import java.util.List;
-
-public interface StationService {
-
-	Boolean createStation(String name, List<String> employeeIds, List<String> stationIds) throws Exception;
-}
