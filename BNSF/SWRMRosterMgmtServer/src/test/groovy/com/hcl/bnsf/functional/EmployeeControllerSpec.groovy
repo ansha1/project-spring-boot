b7ad44f93a7456c312445ff13ca5fbcd50df463f
Sandeep Rajpathak <Sandeep.Rajpathak@hcl.com>
2015-11-06 10:32:46 +0530

    updates

--- BNSF/SWRMRosterMgmtServer/src/test/groovy/com/hcl/bnsf/functional/EmployeeControllerSpec.groovy
+++ BNSF/SWRMRosterMgmtServer/src/test/groovy/com/hcl/bnsf/functional/EmployeeControllerSpec.groovy
@@ -1,28 +1,23 @@
 package com.hcl.bnsf.functional
 
-/**
-import spock.lang.Specification
-
-class HelloControllerSpec extends Specification{
-
-   void "testing spock works"(){
-	   expect:
-	   true
-   }
-
-}
-**/
-
 import org.springframework.boot.SpringApplication
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.boot.test.IntegrationTest
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.SpringApplicationContextLoader
+
 import com.hcl.bnsf.domain.Employee
 import com.hcl.bnsf.service.impl.EmployeeServiceImpl;
 import com.hcl.bnsf.controller.EmployeeController;
+import com.hcl.bnsf.Application
 
+import spock.lang.AutoCleanup;
 import spock.lang.Shared
 import spock.lang.Specification
  
@@ -30,8 +25,57 @@ import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
+
+import groovyx.net.http.RESTClient;
  
+
+@ContextConfiguration(loader = SpringApplicationContextLoader.class, classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:8000")
+
 class EmployeeControllerSpec extends Specification {
+/**	
+
+	@Shared
+	@AutoCleanup
+	def restClient;
+	def headers = ['Content-Type': 'application/json', 'Accept': 'application/json']
+	
+	def setup() {
+		if(restClient == null){
+			 restClient =  new RESTClient("http://localhost:8000")
+		}
+	}
+	
+	def cleanup() {
+	//restClient =  null
+	}
+
+	def setupSpec() {}
+	
+	def cleanupSpec() {
+		restClient =  null
+	}
+	
+	void "test creating a Employee"() {
+		
+		given:
+		
+		when:
+		def response = restClient.post(path: '/BNSF/employee/addEmployeeToRoster', body: 
+			[
+				"id":"1",
+				"rosterId":"123",
+				"name":"Neeraj",
+				"seqNo":123,
+				"phoneNo":"9847484"
+			])
+
+		then:
+		response.status == 200
+		}
+}
+**/
 
 //This test case fails as it is not able to invoke the controller directly, should be invoked via REST URL?  
 /**
@@ -49,6 +93,9 @@ class EmployeeControllerSpec extends Specification {
 		   	addEmployeeToRoster == "Success"
 	   }
 **/
+	
+//This is the dummy test case
+
 //This is copy of unit test case for EmployeeServiceImpl class just so that test case passes
 	EmployeeServiceImpl esi = new EmployeeServiceImpl();
 	
@@ -63,3 +110,4 @@ class EmployeeControllerSpec extends Specification {
 			   addEmployeeToRoster == "Success"
 	   }
 }
+
