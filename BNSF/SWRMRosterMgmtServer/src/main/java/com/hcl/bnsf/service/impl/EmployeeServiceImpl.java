a1ae745ef9d74553fa5043d78b1d6de45890d84f
Neeraj <saxenan@hcl.com>
2015-11-10 10:17:03 +0530

    Changes for critical bugs

--- BNSF/SWRMRosterMgmtServer/src/main/java/com/hcl/bnsf/service/impl/EmployeeServiceImpl.java
+++ BNSF/SWRMRosterMgmtServer/src/main/java/com/hcl/bnsf/service/impl/EmployeeServiceImpl.java
@@ -5,25 +5,14 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hcl.bnsf.domain.Employee;
 import com.hcl.bnsf.service.EmployeeService;
-
-
-import javax.inject.Inject;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 @Service
 @Validated
@@ -32,7 +21,41 @@ public class EmployeeServiceImpl extends GenericServiceClient implements Employe
 
 
 	public String addEmployeeToRoster(Employee employee) throws Exception {
-			return "true";
+		// TODO Auto-generated method stub
+		LOGGER.debug("Inside addEmployeeToRoster");
+		loadProperties();
+		DefaultHttpClient httpClient = null;
+		try {
+			String method = "addEmployeeToRoster";
+			httpClient = new DefaultHttpClient();
+			HttpPost postRequest = new HttpPost(url + "/employee/" + method);
+			LOGGER.debug("END POINT URL: " + postRequest.getURI());
+			String body = OBJECT_MAPPER.writeValueAsString(employee);
+			LOGGER.debug("Request body: " + body);
+			StringEntity entity = new StringEntity(body);
+			entity.setContentType("application/json");
+			postRequest.setEntity(entity);
+			HttpResponse response = httpClient.execute(postRequest);
+			String string = IOUtils.toString(response.getEntity().getContent());
+			System.out.println("Response body: " + string);
+			LOGGER.debug("Response body: " + string);
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + response.getStatusLine().getStatusCode());
+			}
+			System.out.println("Employee Done");
+			httpClient.getConnectionManager().shutdown();
+			return string;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}finally {
+			
+			httpClient.close();
+		}
+		return null;
 	} 
-
 		}
